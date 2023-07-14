9e65d0b0152eed8f8acc858f18be6fd07c755f0c
Heejin Kim <heeezing@gmail.com>
2023-07-14 23:04:35 +0900

    spring

---
+++ ch20MybatisBoot/src/main/java/kr/spring/Ch20MybatisBootApplication.java
@@ -0,0 +1,13 @@
+package kr.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch20MybatisBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch20MybatisBootApplication.class, args);
+	}
+
+}
